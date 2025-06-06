e77a4ec3abd448c812337bd818a16794d0964648
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-06 19:51:48 +0100

    (3/7) Required test

--- Java/MenstrualAppTest.java
+++ Java/MenstrualAppTest.java
@@ -5,8 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MenstrualAppTest{
 
 
+	@Test
+    	void testtGetDateOfCurrentMenstrualPeriodEndWorks() {
+	
+        LocalDate actualDate = MenstrualApp.getDateOfCurrentMenstrualPeriodEnd(LocalDate.of(2025, 8, 3), 26);
+        LocalDate expectedDate = LocalDate.of(2025, 8, 28);
 
+        assertEquals(expectedDate, actualDate);
+    }
 
+	@Test
+    	void testtGetFlowDayEnd() {
+	
+        LocalDate actualDate = MenstrualApp.getFlowDayEnd(LocalDate.of(2025, 8, 3), 7);
+        LocalDate expectedDate = LocalDate.of(2025, 8, 10);
+
+        assertEquals(expectedDate, actualDate);
+    }
+
+	@Test
+    	void testtGetDateOfPredictFetileDayMiddle() {
+	
+        LocalDate actualDate = MenstrualApp.getDateOfPredictFetileDayMiddle(LocalDate.of(2025, 8, 28));
+        LocalDate expectedDate = LocalDate.of(2025, 8, 14);
+
+        assertEquals(expectedDate, actualDate);
+    }
 
 
 
