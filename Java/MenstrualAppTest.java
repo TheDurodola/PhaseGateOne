088b4a5a73f4b9b17e08915e700e50fa417a780d
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-06 13:38:42 +0100

    Create MenstrualAppTest.java

---
+++ Java/MenstrualAppTest.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MenstrualAppTest{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
