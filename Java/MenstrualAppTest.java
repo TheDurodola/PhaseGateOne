540fb8f1a49b34d8340aea83f585e13659727c19
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-06 22:02:36 +0100

    Completed Test

--- Java/MenstrualAppTest.java
+++ Java/MenstrualAppTest.java
@@ -15,6 +15,16 @@ public class MenstrualAppTest{
     }
 
 	@Test
+    	void testtGetDateOfCurrentMenstrualPeriodEndRejectsNegativeValues() {
+	
+        LocalDate actualDate = MenstrualApp.getDateOfCurrentMenstrualPeriodEnd(LocalDate.of(2025, 8, 3), -5);
+        LocalDate expectedDate = null;
+
+        assertEquals(expectedDate, actualDate);
+    }
+
+
+	@Test
     	void testtGetFlowDayEnd() {
 	
         LocalDate actualDate = MenstrualApp.getFlowDayEnd(LocalDate.of(2025, 8, 3), 7);
@@ -33,10 +43,45 @@ public class MenstrualAppTest{
     }
 
 
+	@Test
+    	void testtGetStartDayFertile() {
+	
+        LocalDate actualDate = MenstrualApp.getStartDayFertile(LocalDate.of(2025, 8, 28));
+        LocalDate expectedDate = LocalDate.of(2025, 8, 26);
+
+        assertEquals(expectedDate, actualDate);
+    }
+
+
+	@Test
+    	void testtGetEndDayFertile() {
+	
+        LocalDate actualDate = MenstrualApp.getEndDayFertile(LocalDate.of(2025, 8, 28));
+        LocalDate expectedDate = LocalDate.of(2025, 8, 30);
+
+        assertEquals(expectedDate, actualDate);
+    }
 
 
 
+	@Test
+    	void testtGetSafeDayEnd() {
+	
+        LocalDate actualDate = MenstrualApp.getSafeDayEnd(LocalDate.of(2025, 8, 28));
+        LocalDate expectedDate = LocalDate.of(2025, 8, 27);
 
+        assertEquals(expectedDate, actualDate);
+    }
+
+
+	@Test
+    	void testgetDateOfCurrentMenstrualPeriodEnd() {
+	
+        LocalDate actualDate = MenstrualApp.getDateOfCurrentMenstrualPeriodEnd(LocalDate.of(2025, 8, 28));
+        LocalDate expectedDate = LocalDate.of(2025, 8, 29);
+
+        assertEquals(expectedDate, actualDate);
+    }
 
 
 
