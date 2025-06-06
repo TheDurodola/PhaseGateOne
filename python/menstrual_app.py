25f837df68f3d0e58de6f01eb9f73eb9f875e1e3
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-06 23:59:24 +0100

    Refactored

--- python/menstrual_app.py
+++ python/menstrual_app.py
@@ -1,39 +1,38 @@
 from datetime import date, timedelta
-
-"""duration_of_cycle = 26
-yyyy = 2025
-mm = 8
-dd = 3
-"""
-
-	def get_date_of_current_menstrual_cycle_end_date(yyyy,mm,dd,duration):
+	
+def get_date_of_current_menstrual_cycle_end_date(yyyy,mm,dd,duration_of_cycle):
+	if yyyy >2024 and mm>0 and mm<13 and dd>0 and dd<32:
 		current_cycle_start_date = date(yyyy, mm, dd)
 		current_cycle_end = current_cycle_start_date + timedelta(days=duration_of_cycle-1)
 		return current_cycle_end 
 
-	def get_date_of_flow_day_end():
-		current_cycle_start_date = date(2025, 8, 3)
-		current_flow_end = current_cycle_start_date + timedelta(days=7)
-		return current_flow_end 
+def get_date_of_flow_day_end(yyyy,mm,dd, flow_days):
+	current_cycle_start_date = date(yyyy, mm, dd)
+	current_flow_end = current_cycle_start_date + timedelta(days=flow_days)
+	return current_flow_end 
 
-	
-	def get_date_of_predicted_fertile_day_middle():
-		current_cycle_end = date(2025, 8, 28)
-		fertile_day_middle = current_cycle_end - timedelta(days=14)
-		return fertile_day_middle
-
-	def get_start_day_fertile():
-		fertile_day_middle = date(2025, 8, 14)
-		start_day = fertile_day_middle- timedelta(days=2)
-		return start_day
+def get_start_day_fertile(yyyy, mm, dd, duration_of_cycle):
+	current_cycle_start_date = date(yyyy, mm, dd)
+	current_cycle_end = current_cycle_start_date + timedelta(days=duration_of_cycle-1)
+	fertile_day_middle = current_cycle_end - timedelta(days=14)
+	start_day = fertile_day_middle- timedelta(days=2)
+	return start_day
 
 
-	def get_end_day_fertile():
-		fertile_day_middle = date(2025, 8, 14)
-		end_day = fertile_day_middle + timedelta(days=2)
-		return end_day
+def get_end_day_fertile(yyyy,mm,dd,duration_of_cycle):
+
+	current_cycle_start_date = date(yyyy, mm, dd)
+	current_cycle_end = current_cycle_start_date + timedelta(days=duration_of_cycle-1)
+	fertile_day_middle = current_cycle_end - timedelta(days=14)
+	end_day = fertile_day_middle + timedelta(days=2)
+	return end_day
 
 
-	def get_safe_day_after_ovulation():
-		current_cycle_start_end = date(2025, 8, 14)
-		safe_day = current_cycle_start_end + timedelta(days=3)
+def get_safe_day_after_ovulation(yyyy,mm,dd,duration_of_cycle):
+	current_cycle_start_date = date(yyyy, mm, dd)
+	current_cycle_end = current_cycle_start_date + timedelta(days=duration_of_cycle-1)
+	fertile_day_middle = current_cycle_end - timedelta(days=14)
+	end_day = fertile_day_middle + timedelta(days=2)
+	safe_day = end_day + timedelta(days=1)
+	return safe_day
+
