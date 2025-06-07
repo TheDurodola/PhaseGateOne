0583aa4363f61886910c43ebc98ccba50b4c10cd
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-07 20:00:40 +0100

    (5/5) Functions completed

--- Version 2.0/menstrual_app_functions.py
+++ Version 2.0/menstrual_app_functions.py
@@ -1,5 +1,6 @@
 from datetime import date, timedelta
 
+
 def get_date_of_current_menstrual_cycle_end_date(yyyy,mm,dd,duration_of_cycle):
 	if duration_of_cycle>0:
 		current_cycle_start_date = date(yyyy, mm, dd)
@@ -13,3 +14,15 @@ def get_date_of_end_of_flow_day(yyyy,mm,dd,duration_of_flow):
 		return current_flow_end 
 
 
+def get_start_date_of_ovulation(current_cycle_end):
+	date_of_ovulation_start = current_cycle_end - timedelta(16)
+	return date_of_ovulation_start
+
+def get_end_date_of_ovulation(current_cycle_end):
+	date_of_ovulation_end = current_cycle_end - timedelta(12)
+	return date_of_ovulation_end
+	
+
+def get_safe_day_start_after_ovulation(date_of_ovulation_end):
+	date_of_safe_date_after_ovulation = date_of_ovulation_end +timedelta(1)
+	return date_of_safe_date_after_ovulation
