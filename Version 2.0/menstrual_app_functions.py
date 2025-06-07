b007554b49ab5df2eff882595f0747446f856da7
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-07 21:21:44 +0100

    Change the argument to take in datetime.date() instead of taking three integers representing year, month, day respectively

--- Version 2.0/menstrual_app_functions.py
+++ Version 2.0/menstrual_app_functions.py
@@ -1,16 +1,14 @@
 from datetime import date, timedelta
 
 
-def get_date_of_current_menstrual_cycle_end_date(yyyy,mm,dd,duration_of_cycle):
+def get_date_of_current_menstrual_cycle_end_date(date_that_cycle_started,duration_of_cycle):
 	if duration_of_cycle>0:
-		current_cycle_start_date = date(yyyy, mm, dd)
-		current_cycle_end = current_cycle_start_date + timedelta(days=duration_of_cycle-1)
+		current_cycle_end = date_that_cycle_started + timedelta(days=duration_of_cycle-1)
 		return current_cycle_end 
 
-def get_date_of_end_of_flow_day(yyyy,mm,dd,duration_of_flow):
+def get_date_of_end_of_flow_day(date_that_cycle_started,duration_of_flow):
 	if duration_of_flow>0:
-		current_cycle_start_date = date(yyyy, mm, dd)
-		current_flow_end = current_cycle_start_date + timedelta(days=duration_of_flow)
+		current_flow_end = date_that_cycle_started + timedelta(days=duration_of_flow)
 		return current_flow_end 
 
 
