d192fcca35439aa93abe8fc2ee9ec82bf0e227f8
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-07 20:00:14 +0100

    (7/7) Tests completed

--- Version 2.0/menstrual_app_test.py
+++ Version 2.0/menstrual_app_test.py
@@ -24,3 +24,20 @@ class TestBankApp(TestCase):
 		actual = menstrual_app_functions.get_date_of_end_of_flow_day(2025, 8, 3, -7);
 		expected = None
 		self.assertEqual(actual, expected)
+
+	def test_that_get_start_of_fertile_window_works(self):
+		actual = menstrual_app_functions.get_start_date_of_ovulation(datetime.date(2025, 8, 28));
+		expected = datetime.date(2025, 8, 12)
+		self.assertEqual(actual, expected)		
+
+	def test_that_get_end_of_fertile_window_works(self):
+		actual = menstrual_app_functions.get_end_date_of_ovulation(datetime.date(2025, 8, 28));
+		expected = datetime.date(2025, 8, 16)
+		self.assertEqual(actual, expected)
+
+	def test_that_safe_day_after_ovulation(self):
+		actual = menstrual_app_functions.get_safe_day_start_after_ovulation(datetime.date(2025, 8, 28));
+		expected = datetime.date(2025, 8, 29)
+		self.assertEqual(actual, expected)
+
+
