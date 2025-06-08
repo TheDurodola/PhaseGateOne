dfd1483af12fbd1d3c4fea08fa54eafeb0621b50
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-08 14:20:46 +0100

    Bug discovered in the getSubTotalAmount functions

--- Java/CheckOutAppFunctions.java
+++ Java/CheckOutAppFunctions.java
@@ -28,6 +28,7 @@ public class CheckOutAppFunctions{
 	
 	static int getSubTotalAmount(ArrayList<Integer> itemPriceList , ArrayList<Integer> itemQuantityList){
 	int total = 0;
+	System.out.print(itemPriceList.size());
 	for(int index = 0; index < itemPriceList.size(); index++){
 	total = total + (itemPriceList.get(index) * itemQuantityList.get(index));
 	}
@@ -45,6 +46,20 @@ public class CheckOutAppFunctions{
 	return discountToBeDeducted;
 	}
 
+	static double getVATAmount(ArrayList<Integer> itemPriceList , ArrayList<Integer> itemQuantityList, int discountInPercentage){
+	
+	int totalAmount = 0;
+	for(int index = 0; index < itemPriceList.size(); index++){
+	totalAmount = totalAmount + (itemPriceList.get(index) * itemQuantityList.get(index));
+	}
+	
+	int discountToBeDeducted = totalAmount * discountInPercentage/100;
+	double discountAmount = totalAmount - discountToBeDeducted;
+
+	double amountVAT = discountAmount * 0.075;
+	return amountVAT;
+	}
+	
 	static double getBillTotal(ArrayList<Integer> itemPriceList , ArrayList<Integer> itemQuantityList, int discountInPercentage){
 	
 	int totalAmount = 0;
@@ -56,7 +71,7 @@ public class CheckOutAppFunctions{
 	double discountAmount = totalAmount - discountToBeDeducted;
 
 	double amountVAT = discountAmount * 0.075;
-	double amountAfterVAT = discountAmount - amountVAT;
+	double amountAfterVAT = discountAmount + amountVAT;
 	return amountAfterVAT;
 	}
 
@@ -73,15 +88,15 @@ public class CheckOutAppFunctions{
 	double discountAmount = totalAmount - discountToBeDeducted;
 
 	double amountVAT = discountAmount * 0.075;
-	double amountAfterVAT = discountAmount - amountVAT;
+	double amountAfterVAT = discountAmount + amountVAT;
 
 	double balance = AmoundPaidByCustomer - amountAfterVAT;
 	return balance;
 	}
 
 	
-	static void printInvoice(String nameOfCashier,String nameOfCustomer, ArrayList<String> itemNameList, ArrayList<Integer> itemQuantityList, ArrayList<Integer> itemPriceList, ArrayList<Integer> itemTotalAmount){
-	System.out.print("SEMICOLON STORES\nMAIN BRANCH\nLOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.\nTEL: 080419419419\nDate: 18-Dec-25 8:45:32 PM\n");
+	static void printInvoice(String nameOfCashier,String nameOfCustomer, ArrayList<String> itemNameList, ArrayList<Integer> itemQuantityList, ArrayList<Integer> itemPriceList, ArrayList<Integer> itemTotalAmount, int discountInPercentage){
+	System.out.print("\n\nSEMICOLON STORES\nMAIN BRANCH\nLOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.\nTEL: 080419419419\nDate: 18-Dec-25 8:45:32 PM\n");
 	System.out.printf("Cashier: %s%n", nameOfCashier);
 	System.out.printf("Customer name: %s%n", nameOfCustomer);
 		System.out.print("==========================================================\n");
@@ -90,11 +105,21 @@ public class CheckOutAppFunctions{
 	for(int index = 0; index < itemNameList.size(); index++){
 	System.out.printf("\t\t\t%s\t%d\t%d\t%d%n", itemNameList.get(index), itemPriceList.get(index), itemQuantityList.get(index), itemTotalAmount.get(index)); 
 	}
-System.out.println("----------------------------------------------------------");
+System.out.println("\n----------------------------------------------------------");
 System.out.printf("\t\t\t\t\tSubTotal:%d\n", getSubTotalAmount(itemPriceList, itemQuantityList));
-System.out.printf("\t\t\t\t\tDiscount:%.2f\n", getDiscountedAmount(itemPriceList, itemQuantityList, 8));
-System.out.printf("\t\t\t\tVAT @ 7.5 %.2f", getBillTotal(itemPriceList, itemQuantityList, 8));
-
+System.out.printf("\t\t\t\t\tDiscount:%.2f\n", getDiscountedAmount(itemPriceList, itemQuantityList, discountInPercentage));
+System.out.printf("\t\t\t\t\tVAT @ 7.5 %.2f\n", getVATAmount(itemPriceList, itemQuantityList, discountInPercentage));
+System.out.print("==========================================================\n");
+System.out.printf("\t\t\t\t      Bill Total: %.2f", getBillTotal(itemPriceList, itemQuantityList, discountInPercentage));
+System.out.print("\n==========================================================\n");
+System.out.printf("THIS IS NOT A RECEIPT. KINDLY PAY NGN%.2f", getBillTotal(itemPriceList, itemQuantityList, discountInPercentage));
+System.out.print("\n==========================================================\n");
 	}
-	
+
+
+
+
+
+
+
 }
