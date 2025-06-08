4b14553a7d88c0d81154a86612666bb00fe9d9a3
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-08 08:51:14 +0100

    test that confirms that the functions  testSaveItemQuantityWork and testThatGetTotalAmountWorks work

--- Java/CheckOutAppTest.java
+++ Java/CheckOutAppTest.java
@@ -10,7 +10,6 @@ public class CheckOutAppTest{
 	ArrayList<String> itemNames = new ArrayList<>();
 	ArrayList<String> actual = CheckOutAppFunctions.addItemNameToItemNamesList("Apple", itemNames);
 	
-
 	ArrayList<String> expected = new ArrayList<String>();
 	expected.add("Apple");
 	assertEquals(actual, expected);
@@ -21,11 +20,41 @@ public class CheckOutAppTest{
 	ArrayList<Integer> itemPrices = new ArrayList<>();
 	ArrayList<Integer> actual = CheckOutAppFunctions.addItemPriceToItemPriceList(1500, itemPrices);
 	
-
 	ArrayList<Integer> expected = new ArrayList<>();
 	expected.add(1500);
 	assertEquals(actual, expected);
 	}
+
+
+	@Test
+	void testSaveItemQuantityWork(){
+	ArrayList<Integer> itemQuantity = new ArrayList<>();
+	ArrayList<Integer> actual = CheckOutAppFunctions.addItemQuantityToItemQuantityList(2, itemQuantity);
+	
+	ArrayList<Integer> expected = new ArrayList<>();
+	expected.add(2);
+	assertEquals(actual, expected);
+	}
+	
+	@Test
+	void testThatGetTotalAmountWorks(){
+	ArrayList<Integer> itemQuantity = new ArrayList<>();
+	ArrayList<Integer> itemPrices = new ArrayList<>();
+	itemQuantity.add(2);
+	itemPrices.add(1500);
+	itemQuantity.add(3);
+	itemPrices.add(2000);
+
+
+	int actual = CheckOutAppFunctions.getTotalAmount(itemQuantity, itemPrices);
+	int expected = 9000;
+	
+	assertEquals(actual, expected);
+	}
+
+
+
+
 
 
 
