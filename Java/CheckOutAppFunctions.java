3b22447b5816b8b00b22bee156b0bf908f314f9e
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-08 07:01:14 +0100

    Create CheckOutAppFunctions.java

---
+++ Java/CheckOutAppFunctions.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class CheckOutAppFunctions{
+	
+	static ArrayList<String> addItemNameToItemList(String itemName, ArrayList<String> itemNameLists){
+	itemNameLists.add(itemName);
+	return itemNameLists;
+	}
+
+
+
+}
