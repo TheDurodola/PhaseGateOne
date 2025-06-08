27cc513070b286aef5689963595c1f1512021396
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-08 09:26:27 +0100

    Update CheckOutApp.java

---
+++ Java/CheckOutApp.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class CheckOutApp{
+
+public static void main(String...args){
+
+ArrayList<String> itemNames = new ArrayList<>();
+ArrayList<Integer> itemPrice = new ArrayList<>();
+ArrayList<Integer> itemQuantity = new ArrayList<>();
+int [] totalAmount = new int[1];
+
+
+
+}
+}
