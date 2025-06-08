7284bf62f39d7e61037fd05482a600c975c20e8d
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-08 07:54:19 +0100

    added a functions that stores the name of item to an ArrayList of String and another that stores the prices of item to an ArrayList of Integer

--- Java/CheckOutAppFunctions.java
+++ Java/CheckOutAppFunctions.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 public class CheckOutAppFunctions{
 	
-	static ArrayList<String> addItemNameToItemList(String itemName, ArrayList<String> itemNameLists){
-	itemNameLists.add(itemName);
-	return itemNameLists;
+	static ArrayList<String> addItemNameToItemNamesList(String itemName, ArrayList<String> itemNameList){
+	itemNameList.add(itemName);
+	return itemNameList;
 	}
 
-
+	static ArrayList<Integer> addItemPriceToItemPriceList(int itemPrice, ArrayList<Integer> itemPriceList){
+	itemPriceList.add(itemPrice);
+	return itemPriceList;
+	}
 
 }
