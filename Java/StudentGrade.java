874fa1f2cee7cc2f968751d88f66d4295fd48976
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-13 09:21:19 +0100

    bug fixed

--- Java/StudentGrade.java
+++ Java/StudentGrade.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class StudentGrade{
 
@@ -14,15 +15,15 @@ public class StudentGrade{
 
 
 	for(int counter = 0; counter < array.length; counter++){
-		for(int index = 0; index < array.length; index++){
-			System.out.printf("Entering score for student %d\n", counter);
-			System.out.printf("Enter score for subject %d", index);
+		for(int index = 0; index < array[0].length; index++){
+			System.out.printf("Entering score for student %d\n", counter+1);
+			System.out.printf("Enter score for subject %d\n", index+1);
 			int score = input.nextInt();
 			array[counter][index]= score;	
 		}
 	}	
 
-
+	System.out.print(Arrays.deepToString(array));
 
 
 	}
--- Java/StudentGradeFunctions.java
+++ Java/StudentGradeFunctions.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class StudentGradeFunctions{
 	
 	static int[][] saveSubjectAndStudentSize(int numberOfSubjects, int numberOfStudents){
-		int[][] array = new int [numberOfSubjects][numberOfStudents];
+		int[][] array = new int [numberOfStudents][numberOfSubjects];
 		return array;
 	}
 
