1ace9184305c3adb1b662acae470334b59c6aebb
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-13 11:29:43 +0100

    Constraint of 0-100 Score implemented

--- Java/StudentGrade.java
+++ Java/StudentGrade.java
@@ -13,13 +13,17 @@ public class StudentGrade{
 	
 	int [][] array = StudentGradeFunctions.saveSubjectAndStudentSize(subjectSize, studentSize);
 
-
+	int score = 0;
 	for(int counter = 0; counter < array.length; counter++){
 		for(int index = 0; index < array[0].length; index++){
 			System.out.printf("Entering score for student %d\n", counter+1);
+			while(true){
 			System.out.printf("Enter score for subject %d\n", index+1);
-			int score = input.nextInt();
-			array[counter][index]= score;	
+			score = input.nextInt();
+			if(score>=0 && score<=100){break;}
+			}
+			array[counter][index]= score;
+		System.out.println("\nSaving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");		System.out.println("Saved succesfully");	
 		}
 	}	
 
