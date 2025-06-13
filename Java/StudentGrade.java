bb389148a4698038a672f5a13f632fa9a40b7b18
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-13 14:55:42 +0100

    Update StudentGrade.java

--- Java/StudentGrade.java
+++ Java/StudentGrade.java
@@ -3,31 +3,62 @@ import java.util.Scanner;
 public class StudentGrade{
 
 	public static void main(String[]args){
-	Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 
-	System.out.println("How many students do you have?");
-	int studentSize = input.nextInt();
+		System.out.println("How many students do you have?");
+		int studentSize = input.nextInt();
 
-	System.out.println("How many subjects do they offer?");
-	int subjectSize = input.nextInt();
-	
-	int [][] array = StudentGradeFunctions.saveSubjectAndStudentSize(subjectSize, studentSize);
+		System.out.println("How many subjects do they offer?");
+		int subjectSize = input.nextInt();
+		
+		int [][] array = StudentGradeFunctions.saveSubjectAndStudentSize(subjectSize, studentSize);
 
-	int score = 0;
-	for(int counter = 0; counter < array.length; counter++){
-		for(int index = 0; index < array[0].length; index++){
-			System.out.printf("Entering score for student %d\n", counter+1);
-			while(true){
-			System.out.printf("Enter score for subject %d\n", index+1);
-			score = input.nextInt();
-			if(score>=0 && score<=100){break;}
-			}
+		int score = 0;
+		for(int counter = 0; counter < array.length; counter++){
+			for(int index = 0; index < array[0].length; index++){
+				System.out.printf("Entering score for student %d\n", counter+1);
+				while(true){
+					System.out.printf("Enter score for subject %d\n", index+1);
+					score = input.nextInt();
+					if(score>=0 && score<=100){
+						break;
+					}
+				
 			array[counter][index]= score;
-		System.out.println("\nSaving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");		System.out.println("Saved succesfully");	
+			}
+		System.out.println("\nSaving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");		System.out.println("Saved succesfully\n");	
 		}
-	}	
 
-	System.out.print(Arrays.deepToString(array));
+		System.out.println("=================================================================");
+
+	}	
+	System.out.print("STUDENT	"); 
+	for(int index = 0; index < array[0].length; index++){
+		System.out.printf("SUB%d	", index+1);
+	}
+	System.out.print("TOTAL	AVE	POS");
+
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	}
