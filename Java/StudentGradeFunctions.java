ef8be95b10dd3e9d05875b03465eefab85b485cc
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-13 14:55:46 +0100

    Update StudentGradeFunctions.java

--- Java/StudentGradeFunctions.java
+++ Java/StudentGradeFunctions.java
@@ -7,4 +7,22 @@ public class StudentGradeFunctions{
 	}
 
 
+	static int[] getTotalOfScore(int [][] array){
+	int [] total = new int[array.length];
+	
+	for(int index = 0; index <array.length;index++){
+		int sum = 0;
+		for(int counter=0;counter<array[0].length;counter++){
+		sum = sum + array[index][counter];
+		}
+	 total[index] = sum;
+	}
+	return total;	
+	}
+
+
+
+
+
+
 }
