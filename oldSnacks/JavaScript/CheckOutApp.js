1246648374a076c41b7f79826460f0fc578584c8
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-14 19:21:47 +0100

    Update CheckOutApp.js

--- oldSnacks/JavaScript/CheckOutApp.js
+++ oldSnacks/JavaScript/CheckOutApp.js
@@ -59,3 +59,32 @@ console.log("======================================================");
 console.log(`THIS IS NOT A RECEIPT. KINDLY PAY ${totalBill}`);
 console.log("======================================================");
 
+
+const amountPaid = Number(prompt("How much did the customer give to you: "));
+const balance = amountPaid - totalBill;
+
+console.log("\n\nSEMICOLON STORES\nMAIN BRANCH\nLOCATION: 312, HERBERT MACAULAY WAY, SABO YABA, LAGOS.\nTEL: 080419419419\nDate: 25-DEC-25 8:45:32 PM");
+console.log(`Cashier: ${cashierName}`);
+console.log(`Customer Name: ${customerName}`);
+console.log("======================================================");
+console.log("\t\t\tITEM\tQTY\tPRICE\tTOTAL");
+console.log("======================================================");
+for (let count = 0; count < itemNames.length; count++) {
+      console.log(`\t\t\t${itemNames[count]}\t${itemQuantities[count]}\t${itemPrices[count]}\t${totalOfItem[count]}`);
+};
+console.log("------------------------------------------------------");
+console.log(`\t\t\t\tSubtotal: ${subTotal}`);
+console.log(`\t\t\t\tDiscount: ${discount}`);
+console.log(`\t\t\t\tVAT @ 7.5%: ${vat}`);
+console.log("======================================================");
+console.log(`\t\t\tTotal Bill: ${totalBill}`);
+console.log(`\t\t\tAmount Paid: ${amountPaid}`);
+console.log(`\t\t\tBalance: ${balance}`);
+console.log("======================================================");
+console.log(`\tTHANK FOR YOUR PATRONAGE`);
+console.log("======================================================");
+
+
+
+
+
