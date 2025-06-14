c072ae05110e6b966b7491c2d39f551b6e310bb2
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-14 20:40:13 +0100

    Update testphonebookfunctions.py

--- daySeven/phoneBook/testphonebookfunctions.py
+++ daySeven/phoneBook/testphonebookfunctions.py
@@ -2,4 +2,14 @@ from unittest import TestCase
 from phonebookfunctions import *
 
 class TestPhoneBook(TestCase):
-	def test_that_add_to_phone_book_exists(self):
+	def test_that_add_to_phone_book_works(self):
+		phonebook_database = []
+		actual = add_contact_to_phonebook(phonebook_database, "bolaji", "durodola", 814826070)
+		expected = [["bolaji", "durodola", 814826070]]
+		self.assertEqual(actual, expected)
+
+	def test_that_add_to_phone_book_rejects_(self):
+		phonebook_database = []
+		actual = add_contact_to_phonebook(phonebook_database, "b", "durodola", 814826070)
+		expected = None
+		self.assertEqual(actual, expected)
