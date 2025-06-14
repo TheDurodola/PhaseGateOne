3c5a2a240afd2909b597d13f729357294d7a5d19
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-14 20:45:56 +0100

    Update testphonebookfunctions.py

--- daySeven/phoneBook/testphonebookfunctions.py
+++ daySeven/phoneBook/testphonebookfunctions.py
@@ -8,8 +8,15 @@ class TestPhoneBook(TestCase):
 		expected = [["bolaji", "durodola", 814826070]]
 		self.assertEqual(actual, expected)
 
-	def test_that_add_to_phone_book_rejects_(self):
+	def test_that_add_to_phone_book_rejects_invalid_name_input(self):
 		phonebook_database = []
-		actual = add_contact_to_phonebook(phonebook_database, "b", "durodola", 814826070)
+		actual = add_contact_to_phonebook(phonebook_database, "b", "d", 814826070)
+		expected = None
+		self.assertEqual(actual, expected)
+
+
+	def test_that_add_to_phone_book_rejects_invalid_phone_number_input(self):
+		phonebook_database = []
+		actual = add_contact_to_phonebook(phonebook_database, "Serial", "Killer", 0)
 		expected = None
 		self.assertEqual(actual, expected)
