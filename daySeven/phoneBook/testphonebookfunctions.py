39ad99a4395e754c3babec2a87b5e9aef2479148
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-15 04:03:47 +0100

    Update testphonebookfunctions.py

--- daySeven/phoneBook/testphonebookfunctions.py
+++ daySeven/phoneBook/testphonebookfunctions.py
@@ -5,7 +5,7 @@ class TestPhoneBook(TestCase):
 	def test_that_add_to_phone_book_works(self):
 		phonebook_database = []
 		actual = add_contact_to_phonebook(phonebook_database, "bolaji", "durodola", "08082224444")
-		expected = [["bolaji", "durodola", 814826070]]
+		expected = [["bolaji", "durodola", "08082224444"]]
 		self.assertEqual(actual, expected)
 
 	def test_that_add_to_phone_book_rejects_invalid_name_input(self):
@@ -17,7 +17,7 @@ class TestPhoneBook(TestCase):
 
 	def test_that_add_to_phone_book_rejects_invalid_phone_number_input(self):
 		phonebook_database = []
-		actual = add_contact_to_phonebook(phonebook_database, "Serial", "Killer", 0)
+		actual = add_contact_to_phonebook(phonebook_database, "Serial", "Killer", "0")
 		expected = None
 		self.assertEqual(actual, expected)
 
@@ -36,7 +36,7 @@ class TestPhoneBook(TestCase):
 
 	def test_find_contact_via_phone_number_returns_none_if_number_doesnt_exist(self):
 		phonebook_database =  [["bolaji", "durodola", "08082224444"],["Unilag", "Baddie", "08082223333"]]
-		actual = find_by_phone_number(phonebook_database, 90909090)
+		actual = find_by_phone_number(phonebook_database, "08082224441")
 		expected = None
 		self.assertEqual(actual, expected)
 
