8c58e9e964b336b1d6b41b543ad3a7e5a90472b7
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-15 03:38:22 +0100

    Converted the phone_number variable to be taken in as string adn require a length of 11 digits

--- daySeven/phoneBook/testphonebookfunctions.py
+++ daySeven/phoneBook/testphonebookfunctions.py
@@ -4,13 +4,13 @@ from phonebookfunctions import *
 class TestPhoneBook(TestCase):
 	def test_that_add_to_phone_book_works(self):
 		phonebook_database = []
-		actual = add_contact_to_phonebook(phonebook_database, "bolaji", "durodola", 814826070)
+		actual = add_contact_to_phonebook(phonebook_database, "bolaji", "durodola", "08082224444")
 		expected = [["bolaji", "durodola", 814826070]]
 		self.assertEqual(actual, expected)
 
 	def test_that_add_to_phone_book_rejects_invalid_name_input(self):
 		phonebook_database = []
-		actual = add_contact_to_phonebook(phonebook_database, "b", "d", 814826070)
+		actual = add_contact_to_phonebook(phonebook_database, "", "", "08082224444")
 		expected = None
 		self.assertEqual(actual, expected)
 
@@ -22,7 +22,21 @@ class TestPhoneBook(TestCase):
 		self.assertEqual(actual, expected)
 
 	def test_that_remove_contact_works(self):
-		phonebook_database =  [["bolaji", "durodola", 814826070],["Unilag", "Baddie", 814820666]]
-		actual = remove_contact(phonebook_database, 1)
-		expected =  [["bolaji", "durodola", 814826070]]
+		phonebook_database =  [["bolaji", "durodola", "08082224444"],["Unilag", "Baddie", "08082223333"]]
+		actual = remove_contact(phonebook_database, 2)
+		expected =  [["bolaji", "durodola", "08082224444"]]
 		self.assertEqual(actual, expected)
+
+
+	def test_find_contact_via_phone_number(self):
+		phonebook_database =  [["bolaji", "durodola", "08082224444"],["Unilag", "Baddie", "08082223333"]]
+		actual = find_by_phone_number(phonebook_database, "08082223333")
+		expected = ["Unilag", "Baddie", "08082223333"]
+		self.assertEqual(actual, expected)
+
+	def test_find_contact_via_phone_number_returns_none_if_number_doesnt_exist(self):
+		phonebook_database =  [["bolaji", "durodola", "08082224444"],["Unilag", "Baddie", "08082223333"]]
+		actual = find_by_phone_number(phonebook_database, 90909090)
+		expected = None
+		self.assertEqual(actual, expected)
+
