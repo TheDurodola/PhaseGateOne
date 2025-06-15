e2fb940633135c3be5e3f65e9d3ebe909dc0c43f
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-15 16:46:29 +0100

    Completed (Refactor later)

--- daySeven/phoneBook/Phonebook.java
+++ daySeven/phoneBook/Phonebook.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
+
 public class Phonebook{
+
 	public static void main(String...args){
+	
 	ArrayList<String[]> phonebookDatabase = new ArrayList<>();
 	Scanner input = new Scanner(System.in);
 
@@ -40,17 +44,92 @@ Input: """;
 			PhonebookFunctions.deleteContact(phonebookDatabase, selection);
 		}
 		catch(IndexOutOfBoundsException iobe){
-			System.out.print("INVALID INPUT");
+			System.out.print("ERROR. Invalid Contact selection");
 		}
 		break;
 
 	case 3:
-		System.out.print("Enter the first name: ");
-		String firstNameFind = input.next();
-		PhonebookFunctions.findViaFirstName(phonebookDatabase, firstNameFind);
+		System.out.print("1) Find by First Name\n2) Find by Last Name\n3) Find by Phone Number\n\nEnter your choose: ");
+		int navigate = input.nextInt();
+
+		switch(navigate){
+		case 1:
+			System.out.print("Enter the first name: ");
+			String firstNameFind = input.next();
+			PhonebookFunctions.findViaFirstName(phonebookDatabase, firstNameFind);
+			System.out.print("Enter any key to exit: ");
+			String exit1 = input.next();
+			break;
+		case 2:
+			System.out.print("Enter the last name: ");
+			String lastNameFind = input.next();
+			PhonebookFunctions.findViaLastName(phonebookDatabase, lastNameFind);
+			System.out.print("Enter any key to exit: ");
+			String exit2 = input.next();
+			break;
+		case 3:
+			System.out.print("Enter the phone number: ");
+			String phoneNumberFind = input.next();
+			PhonebookFunctions.findViaPhoneNumber(phonebookDatabase, phoneNumberFind);
+			System.out.print("Enter any key to exit: ");
+			String exit3 = input.next();
+			break;
+			
+		}
+		
+	case 4:
+		for(int index = 0; index < phonebookDatabase.size(); index++){
+		System.out.printf("%d   %s%n", index+1, phonebookDatabase.get(index)[0]);	
+		}
+		System.out.print("What contact would you like to edit: ");
+		int editSelection = input.nextInt();
+		System.out.print("\n\n1) Edit First Name\n2) Edit Last Name\n3) Edit Phone Number\n\nEnter Choose: ");
+		int navi = input.nextInt();
+		switch(navi){
+			case 1: 
+				System.out.print("Enter new first name: ");
+				String newFirstName = input.next();
+				try{
+					PhonebookFunctions.editFirstName(phonebookDatabase, newFirstName, editSelection);
+				}
+				catch(IndexOutOfBoundsException iobe){
+					System.out.print("ERROR. Invalid Contact selection");
+				}
+
+				break;
+
+			case 2: 
+				System.out.print("Enter new last name: ");
+				String newLastName = input.next();
+				try{
+				PhonebookFunctions.editLastName(phonebookDatabase, newLastName, editSelection);}
+				catch(IndexOutOfBoundsException iobe){
+					System.out.print("ERROR. Invalid Contact selection");
+				}
+				break;
+			case 3: 
+				System.out.print("Enter new phone number: ");
+				String newPhoneNumber = input.next();
+				try{
+				PhonebookFunctions.editPhoneNumber(phonebookDatabase, newPhoneNumber, editSelection);}
+				catch(IndexOutOfBoundsException iobe){
+					System.out.print("ERROR. Invalid Contact selection");
+				}
+
+				break;
+			
+
+			default:
+				System.out.print("INVALID INPUT");
+				break;
+
+			}
 		break;
+
+
 	case 0:
 		loopCondition = 1;
+		break;
 	
 	default:
 		System.out.print("INVALID INPUT");
--- daySeven/phoneBook/PhonebookFunctions.java
+++ daySeven/phoneBook/PhonebookFunctions.java
@@ -5,8 +5,8 @@ public class PhonebookFunctions{
 
 	static ArrayList<String[]> addContact(ArrayList<String[]> database, String firstName, String lastName, String phoneNumber){
 		for(int index = 0; index < database.size(); index++){
-			if(phoneNumber == database.get(index)[2]){
-				System.out.print("Contact Number Already Exists");
+			if(phoneNumber.equals(database.get(index)[2])){
+				System.out.print("\nContact Number Already Exists");
 				return database;
 			}
 		}
@@ -15,7 +15,7 @@ public class PhonebookFunctions{
 		if(phoneNumber.length()==11 && !firstName.isBlank()  && !lastName.isBlank()){
 			String [] contactDetails = {firstName, lastName, phoneNumber};
 			database.add(contactDetails);
-			System.out.print("Contact Add\n");
+			System.out.print("Contact Added.\n");
 			return database;
 		}
 		System.out.print("FAILED. Make sure info is valid");
@@ -32,23 +32,59 @@ public class PhonebookFunctions{
 
 	static void findViaFirstName(ArrayList<String[]> database, String firstName){
 	for(int index = 0; index < database.size(); index++){
-			if(firstName == database.get(index)[0]){
-				System.out.printf("First Name: %s\nLast Name: %s\n Phone Number: %s\n", database.get(index)[0],database.get(index)[1],database.get(index)[0]);
+			if(firstName.equals(database.get(index)[0])){
+				System.out.printf("First Name: %s\nLast Name: %s\nPhone Number: %s\n", database.get(index)[0],database.get(index)[1],database.get(index)[2]);
 				return;
 			}
 		}
 
-
+	System.out.print("Contact Doesn't Exist.\n");
 	}
 
 	static void findViaLastName(ArrayList<String[]> database, String lastName){
-
+	for(int index = 0; index < database.size(); index++){
+			if(lastName.equals(database.get(index)[1])){
+				System.out.printf("First Name: %s\nLast Name: %s\nPhone Number: %s\n", database.get(index)[0],database.get(index)[1],database.get(index)[2]);
+				return;
+			}
+		}
+	System.out.print("Contact Doesn't Exist.\n");
 	}
 
 
 	static void findViaPhoneNumber(ArrayList<String[]> database, String phoneNumber){
+	for(int index = 0; index < database.size(); index++){
+			if(phoneNumber.equals(database.get(index)[2])){
+				System.out.printf("First Name: %s\nLast Name: %s\nPhone Number: %s\n", database.get(index)[0],database.get(index)[1],database.get(index)[2]);
+				return;
+			}
+		}
+	System.out.print("Contact Doesn't Exist.\n");
+	}
+
+
+	static ArrayList<String[]> editFirstName(ArrayList<String[]> database, String firstName, int selection){
+	database.get(selection-1)[0] = firstName;
+	System.out.print("Edited.");
+	return database;
 
 	}
+
+
+	static ArrayList<String[]> editLastName(ArrayList<String[]> database, String lastName, int selection){
+	database.get(selection-1)[1] = lastName;
+	System.out.print("Edited.");
+	return database;
+
+	}
+	static ArrayList<String[]> editPhoneNumber(ArrayList<String[]> database, String phoneNumber, int selection){
+	database.get(selection-1)[2] = phoneNumber;
+	System.out.print("Edited.");
+	return database;
+	}
+
+
+
 
 
 }
