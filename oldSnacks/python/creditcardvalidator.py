733f6961237e0fb6d9e2f75e5ef9bc31f7e75384
Durodola Abolaji <142089766+TheDurodola@users.noreply.github.com>
2025-06-17 07:25:50 +0100

    change the import module to reflect the renamed function file

--- oldSnacks/python/creditcardvalidator.py
+++ oldSnacks/python/creditcardvalidator.py
@@ -1,14 +1,14 @@
-import credit_card_validator_functions
+import creditcardvalidatorfunctions
 
 credit_card_number = int(input("Enter your Card Numbers: "))
 try:
-	sum1 = credit_card_validator_functions.get_sum_of_odd_postioned_numbers(credit_card_number)
-	sum2 = credit_card_validator_functions.get_sum_of_even_postioned_numbers(credit_card_number)
+	sum1 = creditcardvalidatorfunctions.get_sum_of_odd_postioned_numbers(credit_card_number)
+	sum2 = creditcardvalidatorfunctions.get_sum_of_even_postioned_numbers(credit_card_number)
 
-	credit_card_validator_functions.get_issuer(credit_card_number)
+	creditcardvalidatorfunctions.get_issuer(credit_card_number)
 	print("**Credit Card Number:",credit_card_number)
 	print("**Credit Card Digit Length:", len(str(credit_card_number)))
-	print("**Credit Card Valid:",credit_card_validator_functions.get_validity(sum1, sum2))
+	print("**Credit Card Valid:",creditcardvalidatorfunctions.get_validity(sum1, sum2))
 
 except TypeError:
 
